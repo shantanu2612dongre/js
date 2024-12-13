49eaa1eb2e607927194cabf4c44dd9fd096cf7de
shantanu2612dongre <129435486+shantanu2612dongre@users.noreply.github.com>
2024-12-13 17:31:19 +0530

    Add files via upload

---
+++ Tic-Tac-Toe/tic-tac-toe.js
@@ -0,0 +1,87 @@
+let boxBtn= document.querySelectorAll(".box");
+let rstBtn = document.querySelector(".reset");
+
+let turnO=true;
+let winningPattern = [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8],
+];
+
+const enableBoxes = () => {
+    for (let box of boxBtn) {
+      box.disabled = false;
+      box.innerText = "";
+    }
+  };
+
+const disableBoxes = () => {
+    for (let box of boxBtn) {
+      box.disabled = true;
+    }
+  };
+
+const resetGame = () => {
+    turnO = true;
+    count = 0;
+    enableBoxes();
+  };
+
+
+
+boxBtn.forEach((box) => {
+    box.addEventListener("click", ()=>{
+        console.log("Box was clicked");
+        if(turnO===true){
+        box.innerText="O";
+        turnO=false;
+        }
+        else{
+            box.innerText="X"
+            turnO=true;
+        }
+        box.disabled=true;
+        winnerCheck();
+       
+    });
+  
+});
+
+let winnerAlert = (winner) => {
+    alert(`winner is,${winner}`);
+    disableBoxes();
+};
+
+let gameDraw = () =>{
+    alert("the game is draw please reset the game to start a new game");
+    disableBoxes();
+};
+
+let winnerCheck=()=>{
+    let isWinner=false;
+    for(pattern of winningPattern){
+        let val1=boxBtn[pattern[0]].innerText;
+        let val2=boxBtn[pattern[1]].innerText;
+        let val3=boxBtn[pattern[2]].innerText;
+        
+        if(val1!= "" && val2!="" && val3!=""){
+            if(val1===val2 && val2===val3){
+                winnerAlert(val1);
+                isWinner=true;
+                return;
+            }
+        }
+        let isDraw = [...boxBtn].every((box) => box.innerText !== "");
+            if (isDraw) {
+                gameDraw();
+                return;
+        }
+    }
+};
+
+rstBtn.addEventListener("click", resetGame);
